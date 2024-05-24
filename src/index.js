3aacab79350c1c17abac2eb09566131b94aba98d
Meet Oza <meet.o@india.zymrinc.com>
2024-05-24 18:48:14 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { store } from './Redux/store';
-import { Provider } from "react-redux";
+// import { store } from './Redux/store';
+// import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       <App />
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>
 
 );
